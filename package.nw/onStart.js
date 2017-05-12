a5ca03ac64f22b644606b5c1ac43630e2c5daa33
undefinedusername <noah.enger@weatherfordisd.net>
2017-05-12 12:51:38 -0500

    Created files

---
+++ package.nw/onStart.js
@@ -0,0 +1,23 @@
+//Runs when you start the Game's exe!
+//
+//
+//-----------------------------------
+
+var cheats={
+	health:,ROT.VK_1,
+	noHunger:ROT.VK_2,
+	devMode:ROT.VK_CONTROL
+}
+var scores={
+     1:1000,
+     2:900,
+     3:800,
+     4:700,
+     5:600
+}
+
+//Self-explainitory
+onRun();
+function onRun(){
+    
+}
